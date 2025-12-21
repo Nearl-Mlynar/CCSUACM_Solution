02fef8eb931aa8e3f9d6307a0187214879360d57
Sakura_luo <2234695291@qq.com>
2025-12-21 20:59:21 +0800

    cf-div4-vp-918

--- CF/code/div4/918/G_Bicycles.cpp
+++ CF/code/div4/918/G_Bicycles.cpp
@@ -5,35 +5,66 @@ using namespace std;
 #define endl '\n'
 const i64 mod = 1e9 + 7;
 const i64 N = 3e5 + 7;
-
-int g[N][N];
+const int64_t inf = 1e18;
 
 void solve()
 {
     int n, m;
     cin >> n >> m;
-
-    fill(&g[0][0], &g[0][0] + N * N, INT_MAX);
-
-    for (int i = 0; i < m; i++)
+    vector<pair<int, int>> g[n];
+    for (int i = 0; i < m; ++i)
     {
         int u, v, w;
         cin >> u >> v >> w;
-
-        g[u][v] = min(g[u][v], w);
-        g[v][u] = min(g[v][u], w);
+        --u, --v;
+        g[u].emplace_back(v, w);
+        g[v].emplace_back(u, w);
     }
 
-    for (int i = 1; i <= n; i++)
+    vector<int> s(n);
+    for (int &i : s)
     {
-        int x;
-        cin >> x;
+        cin >> i;
+    }
 
-        for (int j = 1; j <= n; j++)
+    vector<vector<int64_t>> dist(n, vector<int64_t>(1001, inf));
+    vector<vector<bool>> vis(n, vector<bool>(1001, false));
+
+    dist[0][s[0]] = 0;
+    priority_queue<array<int64_t, 3>> q;
+    q.push({0, 0, s[0]});
+
+    while (!q.empty())
+    {
+        int u = q.top()[1], k = q.top()[2];
+        q.pop();
+
+        if (vis[u][k] || dist[u][k] == inf)
         {
-            
+            continue;
+        }
+
+        vis[u][k] = true;
+
+        for (auto &[v, w] : g[u])
+        {
+            int mm = min(s[v], k);
+            if (dist[v][mm] > dist[u][k] + 1LL * w * k)
+            {
+                dist[v][mm] = dist[u][k] + 1LL * w * k;
+                q.push({-dist[v][mm], v, mm});
+            }
         }
     }
+
+    int64_t res = inf;
+    // 到达终点的摩擦系数不同,时间不同
+    for (int k = 1; k <= 1000; k++)
+    {
+        res = min(res, dist[n - 1][k]);
+    }
+
+    cout << res << endl;
     return;
 }
 
@@ -43,7 +74,7 @@ signed main()
     cin.tie(nullptr);
     cout.tie(0);
     int T = 1;
-    // cin >> T;
+    cin >> T;
     while (T--)
     {
         solve();
